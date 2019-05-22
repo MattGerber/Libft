7066b383476bc44c8efdf1f461d8b76a8227183a
Matthew GERBER <magerber@c3r2s4.wethinkcode.co.za>
2019-05-22 10:32:18 +0200

    Removed the main in strstr and backup for all files

--- ft_strstr.c
+++ ft_strstr.c
@@ -6,7 +6,7 @@
 /*   By: magerber <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/21 15:17:30 by magerber          #+#    #+#             */
-/*   Updated: 2019/05/21 18:12:27 by magerber         ###   ########.fr       */
+/*   Updated: 2019/05/22 10:31:01 by magerber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,16 +35,4 @@ char	*ft_strstr(const char *haystack, const char *needle)
 		haystack++;
 	}
 	return (NULL);
-}
-
-int main () {
-   const char haystack[20] = "TutorialsPoint";
-   const char needle[10] = "Point";
-   char *ret;
-
-   ret = ft_strstr(haystack, needle);
-
-   printf("The substring is: %s\n", ret);
-
-   return(0);
 }

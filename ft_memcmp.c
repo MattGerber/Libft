659140a0795896d8f369d8e098edd82786b6ff68
Matthew GERBER <magerber@c3r2s6.wethinkcode.co.za>
2019-05-23 16:22:58 +0200

    memcmp

---
+++ ft_memcmp.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   memcmp.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: magerber <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/05/23 16:00:54 by magerber          #+#    #+#             */
+/*   Updated: 2019/05/23 16:22:42 by magerber         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int ft_memcmp(const void *str1, const void *str2, size_t n)
+{
+	const unsigned char *s1;
+	const unsigned char *s2;
+	unsigned char c1;
+	unsigned char c2;
+
+	s1 = str1;
+	s2 = str2;
+	c1 = 0;
+	c2 = 0;
+	while (c1 == c2)
+	{
+		c1 = (unsigned char)s1++;
+		c2 = (unsigned char)s2++;
+		if (s1 == '\0')
+			return (c1 - c2);
+	}
+	return (c1 - c2);
+}
